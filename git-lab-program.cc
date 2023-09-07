68849ac6c8271b32637637a60316bfd79b844374
Avery Hauser <143366821+AveryHauser@users.noreply.github.com>
2023-09-07 18:18:17 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <AveryHauser>
+ *        Date: <09/07/2023>
+ * Description: PrograM FOR Lb2 that prints a basic funtion
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
